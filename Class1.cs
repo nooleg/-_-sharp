c95f7791509041c4f6ccd0e17df56580ca0b54d8
nooleg <nooleg@yandex.ru>
2022-02-02 18:01:52 +0500

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Основы_Сsharp
+{
+    internal class Class1
+    {
+    }
+}
